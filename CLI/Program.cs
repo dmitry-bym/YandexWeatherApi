5ddb605e46b35a6429b30c9bb85de44b7cd8f532
Dmitry Kaznacheev <dikaznaceev@gmail.com>
2022-04-13 21:19:02 +1000

    remove

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -5,9 +5,10 @@ using YandexWeatherApi;
 Console.WriteLine("Hello, World!");
 
 var a = await new YandexWeatherServiceBuilder()
-    .UseApiKey("10032850-77da-4064-b2bb-8ec16c3e7ff7")
+    .UseApiKey("")
     .Build()
     .Informers()
     .WithLocality("55.04494", "21.67671")
     .Send(CancellationToken.None);
+
 Console.WriteLine(a.Data.Info.Url);
