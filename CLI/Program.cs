44794a07d77c74f9de811e50a7246db8b53ce780
Dmitry Kaznacheev <dikaznaceev@gmail.com>
2022-04-13 18:46:59 +1000

    fix gitignore

---
+++ CLI/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using YandexWeatherApi;
+
+Console.WriteLine("Hello, World!");
+
+var a = await new YandexWeatherServiceBuilder()
+    .Build()
+    .Informers()
+    .WithLocality("11", "11")
+    .Send(CancellationToken.None);
